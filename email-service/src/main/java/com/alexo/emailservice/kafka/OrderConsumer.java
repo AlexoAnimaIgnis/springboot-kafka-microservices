dcacdf539bda36f566bce346c7c664361b544033
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-10-13 14:27:20 +0800

    implemented email consumer

---
+++ email-service/src/main/java/com/alexo/emailservice/kafka/OrderConsumer.java
@@ -0,0 +1,20 @@
+package com.alexo.emailservice.kafka;
+
+import com.alexo.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent event) {
+        LOGGER.info("Received OrderEvent in email service: {}", event.toString());
+
+        // send email to customer @TODO
+    }
+}
